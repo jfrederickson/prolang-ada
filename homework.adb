9a2e6d5c88b0a0f8053ba48d88bfdc4919a5a0c9
Jonathan Frederickson <silverskullpsu@gmail.com>
2014-03-27 12:05:25 -0400

    Use Unbounded_Strings temporarily since we don't know the input length
    Known issue: must "put" before the loop. Why? No clue.

--- homework.adb
+++ homework.adb
@@ -1,12 +1,15 @@
-with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Strings, Ada.Strings.Fixed;
-use Ada.Text_IO, Ada.Integer_Text_IO, Ada.Strings, Ada.Strings.Fixed;
+with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Strings, Ada.Strings.Fixed, Ada.Strings.Unbounded, Ada.Strings.Unbounded.Text_IO;
+use Ada.Text_IO, Ada.Integer_Text_IO, Ada.Strings, Ada.Strings.Fixed, Ada.Strings.Unbounded, Ada.Strings.Unbounded.Text_IO;
 
 procedure homework is
   
 --   package Str renames Ada.Strings.Unbounded;
   package ITO renames Ada.Integer_Text_IO;
   package SF renames Ada.Strings.Fixed;
+  package UTO renames Ada.Strings.Unbounded.Text_IO;
+  package SU renames Ada.Strings.Unbounded;
   
+  Temp: Unbounded_String;
   outerLine: String(1..15) := (others => ' ');
   curDish: Integer := 0;
   Alice, Bob, Cheryl, Diane, Edgar, expectedDishes:	Integer := 0;
@@ -43,17 +46,32 @@ procedure homework is
 	Edgar := Edgar + 1;
       end if;
       
+    end; --checkLine
+    
+    function toFixedString(str: SU.Unbounded_String) return String is
+    
+    Temp: String := SU.To_String(str);
+    Result: String(1..15);
+    
+    begin
+    SF.Move(source => Temp, Target => Result, Drop => Ada.Strings.Right, Justify => Ada.Strings.Left, Pad => ' ');
+    return Result;
     end;
   
 begin
      
   get(expectedDishes);
 --   put(expectedDishes);
+  Skip_Line;
+  put("Result: ");
+
   loop
     exit when curDish >= expectedDishes;
     outerLine := (others => ' ');
-    Get(outerLine);
---     put_line(outerLine);
+    
+    UTO.Get_Line(Temp);
+    outerLine := toFixedString(Temp);
+    
     checkLine(outerLine);
     outerLine := (1..15 => ' ');
     curDish := curDish + 1;
